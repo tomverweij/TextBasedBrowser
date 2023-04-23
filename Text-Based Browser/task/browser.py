c5a3c09b39db6f4ba5164241ffdc4ca449f735ef
Tom Verweij <tom.verweij@inergy.nl>
2023-04-23 23:58:10 +0200

    end stage 5/6

--- Text-Based Browser/task/browser.py
+++ Text-Based Browser/task/browser.py
@@ -1,18 +1,29 @@
 import sys
 import os
+import shutil
 import requests
 from collections import deque
+from bs4 import BeautifulSoup
 
 dir_name = sys.argv[1]
 
-if not os.path.exists(dir_name):
-    os.mkdir(dir_name)
+if os.path.exists(dir_name):
+    shutil.rmtree(dir_name)
+os.mkdir(dir_name)
 
 history = deque()
 
 
 def get_site_content(url):
-    return requests.get(url).text
+    try:
+        r = requests.get(url)
+        r.encoding = 'UTF-8'
+        soup = BeautifulSoup(r.text, 'html.parser')
+        return '\n'.join([text for text in soup.stripped_strings])
+
+    except requests.exceptions.ConnectionError:
+        print("Invalid URL")
+        return '-1'
 
 
 def save_site_content(data, path):
@@ -21,17 +32,32 @@ def save_site_content(data, path):
         f.write(data)
 
 
-def go_site(site):
-    address = site if site.startswith("https://") else f"https://{site}"
-    site_content = get_site_content(address)
-    save_site_content(site_content, site.removeprefix('https://').split('.')[0])
-    print(site_content)
+def try_site(site):
+    # try reading from cache first
+    site_cache = site.removeprefix('https://').split('.')[0]
+    if site_cache in os.listdir(dir_name):
+        return read_site_from_cache(site_cache)
+
+    # no cache
+    url = site if site.startswith("https://") else f"https://{site}"
+
+    site_content = get_site_content(url)
+    if site_content != '-1':
+        save_site_content(site_content, site_cache)
+        print(site_content)
 
 
 def go_back():
-    history.pop()
-    with open(os.path.join(dir_name, history.pop()), 'r') as f:
-        print(f.read())
+    if len(history) < 2:
+        pass
+    else:
+        history.pop()
+        print(read_site_from_cache(history.pop()))
+
+
+def read_site_from_cache(site_cache):
+    with open(os.path.join(dir_name, site_cache), 'r') as f:
+        return f.read()
 
 
 while True:
@@ -40,7 +66,5 @@ while True:
         break
     if command == "back":
         go_back()
-    elif "." in command:
-        go_site(command)
     else:
-        print("Error: Incorrect URL")
+        try_site(command)
